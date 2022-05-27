e7b585bf3cd0075293ec1950d4fa5e712cfc030c
KkimMinChan <dfmj4520@naver.com>
2022-05-27 19:34:46 +0900

    Create project

---
+++ main.js
@@ -0,0 +1,80 @@
+let str = `
+010-1234-5678
+thesecon@gmail.com
+https://www.omdbapi.com/?apikey=7035c60c&s=frozen
+The quick brown fox jumps over the lazy dog.
+abbcccdddd
+hxyp
+http://localhost:1234
+동해물과_백두산이 마르고 닳도록
+`
+
+
+// const regexp = new RegExp('the', 'gi')
+// console.log(str.match(regexp))
+
+// const regexp = /the/gi
+// console.log(str.match(regexp))
+
+// const regexp = /fox/gi
+// //console.log(regexp.test(str))
+// str = (str.replace(regexp, 'AAA'))
+// console.log(str)
+
+
+// console.log(
+//     str.match(/d$/gm)
+//     )
+// console.log(
+//     str.match(/^t/gim)
+//     )
+
+// console.log(
+//     str.match(/h..p/g)
+// )
+// console.log(
+//     str.match(/fox|dog/)
+// )
+
+// console.log(
+//     str.match(/https?/g)
+// )
+
+// console.log(
+//     str.match(/\b\w{2,3}\b/g)
+// )
+
+// console.log(
+//     str.match(/[가-힣]{1,}/g)
+// )
+
+// console.log(
+//     str.match(/\bf\w{1,}\b/g)
+// )
+
+// console.log(
+//     str.match(/\d{1,}/g)
+// )
+
+// const h = `   the hello  world   !
+// `
+
+// console.log(
+//     h.replace(/\s/g, '')
+// )
+
+const strOne = `
+010-1234-5678
+thesecon@gmail.com
+http://www.omdbapi.com/?apikey=7035c60c&s=frozen
+The quick brown fox jumps over the lazy dog.
+abbcccdddd
+`
+
+// console.log(
+//     strOne.match(/.{1,}(?=@)/g)
+// )
+
+// console.log(
+//     strOne.match(/(?<=@).{1,}/g)
+// )
